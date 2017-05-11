aef56da53c44996ef37c8302af7ad0e9a93a0d25
Gerald Franco Barba <grldfrnc@gmail.com>
2017-05-11 14:51:46 +0200

    Update ArbolNavidad_v2.py

--- ArbolNavidad/ArbolNavidad_v2.py
+++ ArbolNavidad/ArbolNavidad_v2.py
@@ -1,18 +1,19 @@
 
 # coding:utf-8
 
+# DEFINIMOS MI RANGO DE NUMEROS (my_range)
 def my_range(inici, fi, increment):
 	while inici <= fi:
 		yield inici
 		inici = inici + increment
 
-
+# CREAMOS LA ESTRUCTURA (puntos de coordenadas) PARA NUESTRO ARBOL DE NAVIDAD CON FOR ANIDADOS
 for fil in my_range(1, 6, 1):
 	for col in my_range(1, 5, 1):
 		if ( fil==1 ):
 			if ( col==3 ):
 				print "*",
-			else:
+			else:	
 				print " ",
 		else:
 			if ( fil==2 ):
@@ -22,6 +23,7 @@ for fil in my_range(1, 6, 1):
 					print " ",
 			else:
 				if ( fil==3 ):
+					# CONDICIONES PARA CADA COLUMNA DE LA FILA 3
 					if ( col==2 ):
 						print "A",
 					elif ( col==3 ):
