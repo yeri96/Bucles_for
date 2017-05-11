0c16fb2acf13c0bbbf9195212d99b6b99c3c6d99
Gerald Franco Barba <grldfrnc@gmail.com>
2017-05-11 14:39:15 +0200

    Update calendario.py

--- calendario/calendario.py
+++ calendario/calendario.py
@@ -1,18 +1,22 @@
 # coding: utf-8
 
+# DEFINIMOS MI RANGO DE NUMEROS (my_range)
 def my_range(inici,fi,increment):
 	while inici <= fi:
 		yield inici
 		inici = inici + increment
 
+# INICIALIZAMOS EL CONTADOR A 1
 contador=1
-print "L M M J V S D"
+print "L M M J V S D"	# IMPRIMIMOS LA CABECERA
+
+# HACEMOS LA ESTRUCTURA DE NUESTRO CALENDARIO CON FOR ANIDADOS
 for fil in my_range(1,6,1):
 	for col in my_range(1,7,1):
 		if fil==1:
 			if col==7:
 				print contador,
-				contador=contador+1
+				contador=contador+1	# INCREMENTAMOS EL CONTADOR
 			else:
 				print " ",
 		else:
@@ -22,4 +26,4 @@ for fil in my_range(1,6,1):
 					contador=contador+1
 				else:
 					print " ",
-	print ""
+	print ""	# HACEMOS UN SALTO DE LINEA (intro) CADA VEZ QUE SALGAMOS DEL FOR DE COLUMNAS
