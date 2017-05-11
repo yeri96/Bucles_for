4aa0ce2cb346b25199859c43843b80e4ff5b8a89
Gerald Franco Barba <grldfrnc@gmail.com>
2017-05-11 14:57:20 +0200

    Update Tablero1_v2.py

--- Tablero1/Tablero1_v2.py
+++ Tablero1/Tablero1_v2.py
@@ -1,11 +1,12 @@
 # coding:utf-8
 
+# DEFINIMOS NUESTRO RANGO DE NUMEROS
 def my_range(inici, fi, increment):
 	while inici <= fi:
 		yield inici
 		inici = inici + increment
 
-
+# HACEMOS LA ESTRUCTURA DE NUESTRO TABLERO (puntos de coordenadas) CON FOR ANIDADOS
 for fil in my_range(1, 4, 1):
 	for col in my_range(1, 8, 1):
 		if (( fil==1 ) or ( fil==4 )):
@@ -24,9 +25,9 @@ for fil in my_range(1, 4, 1):
 				else:
 					if ( fil==3 ):
 						if ( col==4 ):
-							print "|",
+							print "|",	# AQUI CAMBIAMOS \ POR | PORQUE CONTRABARRA ES UN CARACTER ESPECIAL
 						elif ( col==5 ):
 							print "|",
 						else:
 							print " ",
-print ""
+	print ""	# HACEMOS UN SALTO DE LINEA (intro) CADA QUE SALCA DEL BUCLE DE COLUMNAS
