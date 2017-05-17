6cca65acd4037a9f6c2fe40fb5c2558fbdc38c1e
Gerald Franco Barba <grldfrnc@gmail.com>
2017-05-17 17:24:31 +0200

    Create ListaDinamica1.py

---
+++ ListaDinamica/ListaDinamica1/ListaDinamica1.py
@@ -0,0 +1,10 @@
+# coding: utf-8
+
+
+lista=["L","M","M","J","V","S","D"]		# Definimos la lista
+		
+lista[2]="X"	#Cambiamos los valores de la lista
+lista[0]=0
+lista[6]="domingo"
+
+print lista		# Imprimimos por pantalla la lista
