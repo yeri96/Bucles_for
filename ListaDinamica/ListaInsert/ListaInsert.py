3f0ff32d0e23152ac1289a8ad6a93cddc5bbddf1
Gerald Franco Barba <grldfrnc@gmail.com>
2017-05-17 18:00:52 +0200

    Create ListaInsert.py

---
+++ ListaDinamica/ListaInsert/ListaInsert.py
@@ -0,0 +1,10 @@
+# coding: utf-8
+
+lista=[1,2,3,4,5,6,7]		# Definimos la lista
+
+	# Añadimos el número 9 en la posicion 1 de la lista
+lista.insert(1,9)	
+	# Añadimos las palabras entre comillas en la posicion 3 de la lista
+lista.insert(3,"Posicion 4 de la lista")
+											
+print lista		# Imprimimos por pantalla la lista
