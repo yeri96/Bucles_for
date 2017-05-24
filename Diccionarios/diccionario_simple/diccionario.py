b162e81e811847ee67a8aad7f4e9778a49830422
Gerald Franco Barba <grldfrnc@gmail.com>
2017-05-24 15:15:05 +0200

    Update diccionario.py

--- Diccionarios/diccionario_simple/diccionario.py
+++ Diccionarios/diccionario_simple/diccionario.py
@@ -1,8 +1,8 @@
 # coding: utf-8
 
-frutas = {"fruta1":"naranja","fruta2":"sandia","fruta3":"pera"}
-print frutas["fruta2"]
-print frutas.pop("fruta3")
+frutas = {"fruta1":"naranja","fruta2":"sandia","fruta3":"pera"}   # definimos la lista clave/valor
+print frutas["fruta2"]    # Mostramos por pantalla el valor de la clave fruta2
+print frutas.pop("fruta3")  # Mostramos por pantalla el valor de la clave fruta3 y a la vez lo eliminamos del diccionario
 print "---------------------------------------------------------------------------------------------------------------------------------"
-print frutas
+print frutas  # Mostramos por pantalla el diccionario para ver que se ha eliminado fruta3
 print ""
