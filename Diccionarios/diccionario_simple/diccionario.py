d05ce2caf381d394338a2b0da2c1214038c76776
Gerald Franco Barba <grldfrnc@gmail.com>
2017-05-24 15:07:16 +0200

    Create diccionario.py

---
+++ Diccionarios/diccionario_simple/diccionario.py
@@ -0,0 +1,8 @@
+# coding: utf-8
+
+frutas = {"fruta1":"naranja","fruta2":"sandia","fruta3":"pera"}
+print frutas["fruta2"]
+print frutas.pop("fruta3")
+print "---------------------------------------------------------------------------------------------------------------------------------"
+print frutas
+print ""
